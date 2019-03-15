169444caa6bf3912ccbedb8a969c1fe48a12984f
Leonel Nieto <43765662+leonelnieto@users.noreply.github.com>
2019-03-14 20:37:55 -0600

    Updates

---
+++ precache-manifest.0cf72ac6aae463eb33dd57ba7836ebe5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f20ae6f6f970b023f341",
+    "url": "/spa/static/js/runtime~main.f20bd972.js"
+  },
+  {
+    "revision": "df1e055c99d3b0297199",
+    "url": "/spa/static/js/main.21c064e6.chunk.js"
+  },
+  {
+    "revision": "6bce3fdbf72c0efe5a2b",
+    "url": "/spa/static/js/2.098369f2.chunk.js"
+  },
+  {
+    "revision": "df1e055c99d3b0297199",
+    "url": "/spa/static/css/main.70b022d5.chunk.css"
+  },
+  {
+    "revision": "6c42381c2f1d81d72b69c94f8c442609",
+    "url": "/spa/index.html"
+  }
+];
